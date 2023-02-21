213913ffe2c007354493ea4169773227cfc2780b
jayashreemore <120532991+jayashreemore@users.noreply.github.com>
2023-02-21 13:03:18 -0500

    completed html homework

---
+++ Javascriptfolder/Day1.js
@@ -0,0 +1,29 @@
+
+
+let temp = 60;
+
+if (temp > 45) {
+    console.log("I'm going hiking!");
+} else {
+    console.log("It's too cold to go hiking");
+}
+
+let cups = 5;
+
+if(cups < 3){
+    console.log("Yes. I'll take another cup of coffee!");
+} else {
+    console.log("I think I'm okay for now.");
+}
+
+
+let cups = 5;
+
+if (x <=3 && x >=5){
+    console.log("I ll wait for 30 minutes")
+}
+if(x < 3){
+    console.log("Yes. I'll take another cup of coffee!");
+} else {
+    console.log("I think I'm okay for now.");
+}
